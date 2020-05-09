50c0cbaa36415ce2db385e01a5080ee14dc7c61d
aftiyah <abd2010ly@gmail.com>
2020-05-09 16:19:44 +0200

    Add error handlers.

--- backend/src/api.py
+++ backend/src/api.py
@@ -87,24 +87,27 @@ def unprocessable(error):
                     "message": "unprocessable"
                     }), 422
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+                    "success": False,
+                    'error': 404,
+                    'message': "resource not found"
                     }), 404
 
-'''
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def authorization_failed(error):
+    return jsonify({
+                    "success": False,
+                    'error': error.status_code,
+                    'message': error.error.description #################################
+                    }), error.status_code
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'success': False,
+                    'error': 500,
+                    'message': 'internal server error'
+                    }), 500 
